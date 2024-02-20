7e2a7253f7f22174f8ef7ce7b6043c8f2a2cd209
Lily Whitford <lilywhitford2@gmail.com>
2024-02-20 23:10:17 +0000

    added handleUserInput function to allow played to exit the game with ctrl + c

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const { connect } = require("./client")
 
+// setup interface to handle user input from stdin
+
+const handleUserInput = function (data) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
 console.log("Connecting ...");
 connect();
