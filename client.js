a226fb123d33181ce04cb5346ad24bf758b1182c
Lily Whitford <lilywhitford2@gmail.com>
2024-02-20 01:50:12 +0000

    imported connect function

---
+++ client.js
@@ -0,0 +1,24 @@
+
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  conn.on("connect", () => { // code that does something when the connection is first established
+    console.log("Successfully connected to server!");
+  });
+
+  conn.on("data", (data) => { 
+    console.log(data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
