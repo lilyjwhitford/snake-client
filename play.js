8f2beb216ea7525d91f54528b83c0fe6c521f5b0
Lily Whitford <lilywhitford2@gmail.com>
2024-02-20 23:24:01 +0000

    imported setupInput function

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client")
-
-// setup interface to handle user input from stdin
-
-const handleUserInput = function (data) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
+const { setupInput } = require("./input")
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
