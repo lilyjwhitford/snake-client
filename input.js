05ef543228a29d94455e9745d8ea1e9e2a377000
Lily Whitford <lilywhitford2@gmail.com>
2024-02-21 01:10:23 +0000

    added WASD movement features and updated play.js to pass connection object to setupInput function

--- input.js
+++ input.js
@@ -1,11 +1,24 @@
+let connection; // stores the active TCP connection object
+
 const handleUserInput = function (data) {
-  if (data === "\u0003") { // closes program with ctrl + c
+  if (data === "\u0003") { // terminates program with ctrl + c
     process.exit();
   }
+  if (data === "w" ) {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  } else if (data === "d") {
+    connection.write("Move: right");
+  }
 };
-// setup interface to handle user input from stdin
 
-const setupInput = function () {
+
+// setup interface to handle user input from stdin
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -1,7 +1,8 @@
-const { connect } = require("./client")
-const { setupInput } = require("./input")
+const { connect } = require("./client"); // import connect function from client.js
+const { setupInput } = require("./input"); // import setupInput function from input.js
+
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+const conn = connect(); // store connection object returned by connect in variable
+setupInput(conn); // pass connection object into setupInput function
